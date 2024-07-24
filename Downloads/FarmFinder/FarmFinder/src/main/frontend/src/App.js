20d86b14e054b8f3ff85c081ed4b8aabffb5dd93
Yeezusson21 <kaarthikeyan.veeravelmurugan@sjsu.edu>
2024-07-24 12:26:05 -0700

    topbar

--- Downloads/FarmFinder/FarmFinder/src/main/frontend/src/App.js
+++ Downloads/FarmFinder/FarmFinder/src/main/frontend/src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
+import MenuBar from './components/MenuBar'
 import GenerateGroceryList from './components/GenerateGroceryList'
 import ExploreFarms from './components/ExploreFarms'
 import Layout from './Layout';
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path = "/" element = {<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<GenerateGroceryList />} />
-          <Route path="/contact" element={<ExploreFarms />} />
-        </Route>
-      </Routes>
-    </Router>
+    <div className="App">
+        <div className="logo-container">
+          <div className="brand-logo">
+            <img src = "/logo.png" width = {50} height = {80} alt = "logo" />
+          </div>
+        </div>
+      <header className="App-header">
+        <p>FarmFinder</p>
+      </header>
+      <MenuBar />
+    </div>
   );
 }
 
---
+++ Downloads/FarmFinder/FarmFinder/src/main/frontend/src/components/MenuBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MenuBar = () => {
+  return (
+    <nav className="menu-bar">
+      <ul>
+        <li><a href="#farms">Explore Farms</a></li>
+        <li><a href="#poglg">Premium Organic Grocery List Generator</a></li>
+        <li><a href="#resources">Resources</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact Us</a></li>
+        <li><a href="#login">Login</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MenuBar;
