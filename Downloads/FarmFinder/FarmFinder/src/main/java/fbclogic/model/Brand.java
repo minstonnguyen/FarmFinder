6bd680cfb4d1e1c400c3f2bd83d3768e5ddbb3b9
Minston <minstonnguyen@gmail.com>
2024-08-21 22:47:47 -0700

    created backend structure with controller, model, repository, service

--- Downloads/FarmFinder/FarmFinder/src/main/java/fbclogic/model/Brand.java
+++ Downloads/FarmFinder/FarmFinder/src/main/java/fbclogic/model/Brand.java
@@ -37,4 +37,119 @@ public class Brand {
         this.description = description;
         this.brandName = brandName;
     }
+    public int getBrandID() {
+        return brandID;
+    }
+
+    public void setBrandID(int brandID) {
+        this.brandID = brandID;
+    }
+
+    public int getScorecardID() {
+        return scorecardID;
+    }
+
+    public void setScorecardID(int scorecardID) {
+        this.scorecardID = scorecardID;
+    }
+
+    public int getFarmID() {
+        return farmID;
+    }
+
+    public void setFarmID(int farmID) {
+        this.farmID = farmID;
+    }
+
+    public short getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(short starRating) {
+        this.starRating = starRating;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getProducts() {
+        return products;
+    }
+
+    public void setProducts(String products) {
+        this.products = products;
+    }
+
+    public String getBrandMarketArea() {
+        return brandMarketArea;
+    }
+
+    public void setBrandMarketArea(String brandMarketArea) {
+        this.brandMarketArea = brandMarketArea;
+    }
+
+    public String getOrganic() {
+        return organic;
+    }
+
+    public void setOrganic(String organic) {
+        this.organic = organic;
+    }
+
+    public String getPlantBased() {
+        return plantBased;
+    }
+
+    public void setPlantBased(String plantBased) {
+        this.plantBased = plantBased;
+    }
+
+    public String getParentCompany() {
+        return parentCompany;
+    }
+
+    public void setParentCompany(String parentCompany) {
+        this.parentCompany = parentCompany;
+    }
+
+    public String getCompanyType() {
+        return companyType;
+    }
+
+    public void setCompanyType(String companyType) {
+        this.companyType = companyType;
+    }
+
+    public String getParticipatedInSurvey() {
+        return participatedInSurvey;
+    }
+
+    public void setParticipatedInSurvey(String participatedInSurvey) {
+        this.participatedInSurvey = participatedInSurvey;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+    @Override
+    public String toString(){
+        return "Brand{" + brandName;
+    }
 }
